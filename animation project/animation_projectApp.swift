173839a07038100493bd0d74571d69c99c035568
Elan Tamez <tamezapple@gmail.com>
2025-09-25 14:22:39 -0600

    Initial Commit

---
+++ animation project/animation_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  animation_projectApp.swift
+//  animation project
+//
+//  Created by Tamez, Elan - Student on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct animation_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ animation projectTests/animation_projectTests.swift
@@ -0,0 +1,17 @@
+//
+//  animation_projectTests.swift
+//  animation projectTests
+//
+//  Created by Tamez, Elan - Student on 9/25/25.
+//
+
+import Testing
+@testable import animation_project
+
+struct animation_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
